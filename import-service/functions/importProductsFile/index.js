6c58bcc81f4c130f1e064c338bf7e98045e0c400
Andrii_Kuksovskyi <andrii_kuksovskyi@epam.com>
2023-03-11 19:44:28 +0200

    wip

---
+++ import-service/functions/importProductsFile/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports.handler = async (event) => {
+    try {  
+    //   return {
+    //     statusCode: 200,
+    //     body: JSON.stringify({ message: "Product successfully created." }),
+    //   };
+    } catch (error) {
+    //   return {
+    //     statusCode: 500,
+    //     body:  JSON.stringify( { message: error.message || 'Something went wrong !!!' })
+    //   };
+    }
+  };
