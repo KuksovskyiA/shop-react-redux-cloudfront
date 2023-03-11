2fcb831e5b86c19bcbad2dd1e4134e48b877a4e8
Andrii_Kuksovskyi <andrii_kuksovskyi@epam.com>
2023-03-11 23:48:56 +0200

    wip

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://l91sorqzme.execute-api.eu-west-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://074gu4ea52.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://08apguul84.execute-api.eu-west-1.amazonaws.com",
   bff: "https://l91sorqzme.execute-api.eu-west-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
